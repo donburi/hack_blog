97367cb03e61ac3aef7a154d60df09847699a689
Don Currie <d.currie@utoronto.ca>
2015-03-05 21:20:47 -0500

    added tests

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -4,9 +4,13 @@ class PostTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "lead is the same if lead is less than 100 chatacters" do
+  	p = Post.new(content: "Hello")
+  	assert_equal p.lead, "Hello..."
+  end
 
   test "lead length is 100 characters" do
   	p = Post.new(content: "x" * 200)
-  	assert p.lead.length == 100
+  	assert_equal ("x" * 100) + "...", p.lead
   end
 end
