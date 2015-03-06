9fffb452495887468d61e9949893a259ee0acad2
Don Currie <d.currie@utoronto.ca>
2015-03-05 21:13:59 -0500

    added ==, by?, same_category?

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,17 @@ class Post < ActiveRecord::Base
 	def lead
 		content.first(100) + "..."
 	end
+
+	def ==(b)
+		title == b.title 
+	end
+
+	def by?(author)
+		self.author == author
+	end
+
+	def same_category?(post)
+		self.category == post.category
+	end
+
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -4,4 +4,9 @@ class PostTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "lead length is 100 characters" do
+  	p = Post.new(content: "x" * 200)
+  	assert p.lead.length == 100
+  end
 end
