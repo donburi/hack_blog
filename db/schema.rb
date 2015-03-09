1e9eba57b38a562e2064d5825dde71e64b9407fb
Don Currie <d.currie@utoronto.ca>
2015-03-09 19:06:23 -0400

    add post view

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150306013007) do
+ActiveRecord::Schema.define(version: 20150309225026) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20150306013007) do
     t.string   "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "view_count"
   end
 
 end
