2688f2a15efd2294cbb5c3da163769a8206f2572
Don Currie <d.currie@utoronto.ca>
2015-03-19 16:54:04 -0400

    fixed is_over_18

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ validates :date_of_birth, presence: true
 validate :is_over_18
 
 	def is_over_18
-		if date_of_birth < 18.years.ago
+		if date_of_birth > 18.years.ago
 			errors.add(:date_of_birth, "is less than 18 years ago")
 		end
 	end
